ae8324968286e82268c5892258f67f680bf4c4c1
Duong Dinh Long <duongdinhlonh123@gmail.com>
2024-03-25 18:38:24 +0700

    finish Subject service

---
+++ src/main/java/iuh/registerservice/services/UserService.java
@@ -0,0 +1,26 @@
+package iuh.registerservice.services;
+
+import iuh.registerservice.models.User;
+import iuh.registerservice.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getListUser(){
+        return  userRepository.findAll();
+    }
+
+    public User getUserById(long id){
+        return userRepository.findById(id).get();
+    }
+}
